d6bfed449e9c931a93f52b0a16e018399d50eacf
Obaid Ahmed Khan <57769441+obaidak407@users.noreply.github.com>
2022-10-07 18:06:27 +0500

    Register changes
    Register changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
 <div class="page-content  align-items-center justify-content-center" style="background:#101920">
     <img src="{{ asset('assets/images/logo.png') }}" width="100px" style="display:block;margin:auto; margin-bottom:35px">
     <div>
-        <span style="text-align: center;color:white; font-family: 'akira'; font-size:18px;"><h1 style="font-size:18px">LOGIN TO</h1></span>
+        <span style="text-align: center;color:white; font-family: 'akira'; font-size:18px;"><h1 style="font-size:18px">REGISTER TO</h1></span>
         <span  style="text-align: center;color:#E09946; font-family: 'akira'; font-size:22px; "><h1 style="font-size:22px">WIZZ LOGISTICS</h1></span>
         <div class="row w-100 mx-0 auth-page" style="margin-top:35px">
             <div class="col-lg-4 col-md-9 col-xl-4 mx-auto" >
@@ -42,7 +42,6 @@
                                     </div>
                                     <div class="form-group">
                                         <label style="font-family:Montserrat;color: white;font: size 16px; margin-bottom: 10px;">Password</label>
-                                        <label id="ForgotPassword" style="font-family:Montserrat;color: white;font: size 16px; margin-bottom: 10px; float:right;"><a href="{{url('/forgot-password')}}" style="color:#E09946">Forget Password?</a></label>
                                         <input id="password" type="password" name="password" required placeholder="Enter your password" style="margin-bottom: 20px; background:#101920;border: 1px solid #262F36; border-radius: 5px; height:45px" class="form-control @error('password') is-invalid @enderror">
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
@@ -52,28 +51,28 @@
                                     </div>
                                         
 
+                                    <label style="font-family:Montserrat;color: white;font: size 16px; margin-bottom: 10px;">Account Type</label>
                                     <div class="form-group">
-                                        <label style="font-family:Montserrat;color: white;font: size 16px; margin-bottom: 10px;">Account Type</label>
                                         <div class="form-check form-check-inline">
                                           <label class="form-check-label" style="color:white;">
                                             <input type="radio" class="form-check-input" name="optionsRadios5" id="optionsRadios6" value="option5">
-                                            Default
+                                            User
                                           </label>
                                         </div>
                                         <div class="form-check form-check-inline">
                                             <label class="form-check-label" style="color:white;">
                                               <input type="radio" class="form-check-input" name="optionsRadios5" id="optionsRadios6" value="option5">
-                                              Default
+                                              Vendor
                                             </label>
                                         </div>
                                     </div>
 
 
                                     <div class="mt-3">
-                                        <button type="submit" class="btn btn-primary btn-block" style=" background:#E09946;  border: 1px solid #FFBF74; border-radius: 5px; box-shadow:none; color:#172128; height:45px; font-family:'akira'; font-size:18px">Login</button>
+                                        <button type="submit" class="btn btn-primary btn-block" style=" background:#E09946;  border: 1px solid #FFBF74; border-radius: 5px; box-shadow:none; color:#172128; height:45px; font-family:'akira'; font-size:18px">Register</button>
                                     </div>
                                     <div style="padding-bottom: 0px;">
-                                        <label style="font-family:Montserrat;color: white;font: size 16px; margin-top: 34px; text-align: center;width:100%; marin-bottom:0px;">Don't have an account?<a href="{{url('/register')}}" style="color:#E09946"> Register New</a></label>
+                                        <label style="font-family:Montserrat;color: white;font: size 16px; margin-top: 34px; text-align: center;width:100%; marin-bottom:0px;">Already have an account?<a href="{{url('/register')}}" style="color:#E09946"> Login</a></label>
                                     </div>
                                 </form>
                             </div>
