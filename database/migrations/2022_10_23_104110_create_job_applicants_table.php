163273fa93422ee67f38d6908e95fff5ededabed
Abdul Hadi Butt <hadibutt476@gmail.com>
2022-10-28 17:07:33 +0500

    Jobs Applicants Migration and Model

---
+++ database/migrations/2022_10_23_104110_create_job_applicants_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobApplicantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_applicants', function (Blueprint $table) {
+            $table->id();
+            $table->string('job_id')->nullable();
+            $table->string('ip_address')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('resume')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_applicants');
+    }
+}
