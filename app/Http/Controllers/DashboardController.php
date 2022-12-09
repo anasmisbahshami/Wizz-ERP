7e017638ff60b88c4fc88e8822e8e15b67866c86
Abdul Hadi Butt <hadibutt476@gmail.com>
2022-12-10 03:29:28 +0500

    Dashboard small cards functional

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,18 +4,139 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Order;
+use App\Models\UserSubscription;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use AmrShawky\LaravelCurrency\Facade\Currency;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         if(\Auth::user()->hasAnyRole(['Super Admin','Admin'])){
+            //Intializing Dates
             $start_date = Carbon::now()->subMonth(1)->format('d-F-Y');
             $end_date = Carbon::now()->format('d-F-Y');
-            return view('dashboard.dashboard', compact('start_date', 'end_date'));
+            
+        //++++++++++++++++++++++ Customer Figures Calculation Start ++++++++++++++++++++++//
+            
+            //Last Month Customers
+            $last_month_customers = User::role('User')
+            ->whereBetween('created_at', [Carbon::now()->subMonth(2), Carbon::now()->subMonth(1)])
+            ->count();
+
+            //Current Month Customers
+            $current_month_customers = User::role('User')
+            ->whereBetween('created_at', [Carbon::now()->subMonth(1), Carbon::now()])
+            ->count();
+
+            //Growth Rate of Customers comparing Last Month to Current Month
+            if($last_month_customers == 0){
+                $CustomersGrowthPercentage = 0;
+            }else{
+                $CustomersGrowthPercentage = ($current_month_customers/$last_month_customers)*100;
+            }
+
+            //Calculating Customers Growth Percentage
+            if ($CustomersGrowthPercentage >= 0) {
+                $CustomerSign = 'Positive';
+            }else{
+                $CustomerSign = 'Negative';
+            }
+            
+            $CustomerArray = array(
+                'CurrentMonthCustomers' => $current_month_customers,
+                'CustomersGrowthPercentage' => $CustomersGrowthPercentage,
+                'CustomerSign' => $CustomerSign,
+            );
+
+        //++++++++++++++++++++++ Customer Figures Calculation End ++++++++++++++++++++++//
+
+
+        //++++++++++++++++++++++ Subscription Figures Calculation Start ++++++++++++++++++++++//
+
+            //Last Month Subscriptions
+            $last_month_subscriptions = UserSubscription::whereBetween('created_at', [Carbon::now()->subMonth(2), Carbon::now()->subMonth(1)])
+            ->where('status', 'Subscribed')
+            ->count();
+
+            //Current Month Subscriptions
+            $current_month_subscriptions = UserSubscription::whereBetween('created_at', [Carbon::now()->subMonth(1), Carbon::now()])
+            ->where('status', 'Subscribed')
+            ->count();
+
+            //Growth Rate of Subscriptions comparing Last Month to Current Month
+            if($last_month_subscriptions == 0){
+                $SubscriptionGrowthPercentage = 0;
+            }else{
+                $SubscriptionGrowthPercentage = ($current_month_subscriptions/$last_month_subscriptions)*100;
+            }
+
+            //Calculating Subscription Growth Percentage
+            if ($SubscriptionGrowthPercentage >= 0) {
+                $SubscriptionSign = 'Positive';
+            }else{
+                $SubscriptionSign = 'Negative';
+            }
+            
+            $SubscriptionArray = array(
+                'CurrentMonthSubscriptions' => $current_month_subscriptions,
+                'SubscriptionGrowthPercentage' => $SubscriptionGrowthPercentage,
+                'SubscriptionSign' => $SubscriptionSign,
+            );
+
+        //++++++++++++++++++++++ Subscription Figures Calculation End ++++++++++++++++++++++//
+
+
+        //++++++++++++++++++++++ Revenue Figures Calculation Start ++++++++++++++++++++++//
+
+            //Calculating Last Month Orders
+            $last_month_orders = Order::with('items')->whereBetween('created_at', [Carbon::now()->subMonth(2), Carbon::now()->subMonth(1)])->where('status', 'Complete')->get();
+
+            //Calculating Last Month Orders Revenue
+            $last_month_revenue = 0;
+            foreach ($last_month_orders as $order) {
+                $last_month_revenue += $order->items->sum('price');
+            }
+
+            //Calculating Current Month Orders Revenue
+            $current_month_orders = Order::with('items')->whereBetween('created_at', [Carbon::now()->subMonth(1), Carbon::now()])->where('status', 'Complete')->get();
+
+            //Calculating Current Month Order Revenue
+            $current_month_revenue = 0;
+            foreach ($current_month_orders as $order) {
+                $current_month_revenue += $order->items->sum('price');
+            }
+
+            // Converting PKR to USD Revenue;
+            $CurrentMonthDollarRevenue = Currency::convert()->from('PKR')->to('USD')->amount($current_month_revenue)->get();
+
+            //Growth Rate of Revenue comparing Last Month to Current Month
+            if($last_month_revenue == 0){
+                $RevenueGrowthPercentage = 0;
+            }else{
+                $RevenueGrowthPercentage = ($current_month_revenue/$last_month_revenue)*100;
+            }
+
+            //Calculating Revenue Growth Percentage
+            if ($RevenueGrowthPercentage >= 0) {
+                $RevenueSign = 'Positive';
+            }else{
+                $RevenueSign = 'Negative';
+            }
+
+            $RevenueArray = array(
+                'CurrentMonthDollarRevenue' => $CurrentMonthDollarRevenue,
+                'RevenueGrowthPercentage' => $RevenueGrowthPercentage,
+                'RevenueSign' => $RevenueSign,
+            );
+
+        //++++++++++++++++++++++ Revenue Figures Calculation End ++++++++++++++++++++++//
+
+        return view('dashboard.dashboard', compact('start_date', 'end_date','CustomerArray','SubscriptionArray','RevenueArray'));
+
         }else{
             return redirect('/profile/view');
         }
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -76,11 +76,11 @@
                   </div>
                   <div class="row">
                     <div class="col-6 col-md-12 col-xl-5">
-                      <h3 class="mb-2">3,897</h3>
+                      <h3 class="mb-2">{{number_format($CustomerArray['CurrentMonthCustomers'], 0)}}</h3>
                       <div class="d-flex align-items-baseline">
-                        <p class="text-success">
-                          <span>+3.3%</span>
-                          <i data-feather="arrow-up" class="icon-sm mb-1"></i>
+                        <p @if ($CustomerArray['CustomerSign'] == 'Positive') class="text-success" @else class="text-danger" @endif>
+                          <span>{{number_format($CustomerArray['CustomersGrowthPercentage'], 1)}}%</span>
+                          <i @if ($CustomerArray['CustomerSign'] == 'Positive') data-feather="arrow-up" @else data-feather="arrow-down" @endif  class="icon-sm mb-1"></i>
                         </p>
                       </div>
                     </div>
@@ -111,11 +111,11 @@
                   </div>
                   <div class="row">
                     <div class="col-6 col-md-12 col-xl-5">
-                      <h3 class="mb-2">35,084</h3>
+                      <h3 class="mb-2">{{number_format($SubscriptionArray['CurrentMonthSubscriptions'], 0)}}</h3>
                       <div class="d-flex align-items-baseline">
-                        <p class="text-danger">
-                          <span>-2.8%</span>
-                          <i data-feather="arrow-down" class="icon-sm mb-1"></i>
+                        <p @if ($SubscriptionArray['SubscriptionSign'] == 'Positive') class="text-success" @else class="text-danger" @endif>
+                          <span>{{number_format($SubscriptionArray['SubscriptionGrowthPercentage'], 1)}}%</span>
+                          <i @if ($SubscriptionArray['SubscriptionSign'] == 'Positive') data-feather="arrow-up" @else data-feather="arrow-down" @endif class="icon-sm mb-1"></i>
                         </p>
                       </div>
                     </div>
@@ -130,7 +130,7 @@
               <div class="card">
                 <div class="card-body">
                   <div class="d-flex justify-content-between align-items-baseline">
-                    <h6 class="card-title mb-0">Growth</h6>
+                    <h6 class="card-title mb-0">Revenue</h6>
                     <div class="dropdown mb-2">
                       <button class="btn p-0" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
@@ -146,11 +146,11 @@
                   </div>
                   <div class="row">
                     <div class="col-6 col-md-12 col-xl-5">
-                      <h3 class="mb-2">89.87%</h3>
+                      <h3 class="mb-2">${{number_format($RevenueArray['CurrentMonthDollarRevenue'], 0)}}</h3>
                       <div class="d-flex align-items-baseline">
-                        <p class="text-success">
-                          <span>+2.8%</span>
-                          <i data-feather="arrow-up" class="icon-sm mb-1"></i>
+                        <p @if ($RevenueArray['RevenueSign'] == 'Positive') class="text-success" @else class="text-danger" @endif>
+                          <span>{{number_format($RevenueArray['RevenueGrowthPercentage'], 1)}}%</span>
+                          <i @if ($RevenueArray['RevenueSign'] == 'Positive') data-feather="arrow-up" @else data-feather="arrow-down" @endif class="icon-sm mb-1"></i>
                         </p>
                       </div>
                     </div>
